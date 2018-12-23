1e51db5882799e69ba46073bedb1152bc8a1945c
Albert221 <w.albert221@gmail.com>
2018-12-23 17:20:10 +0100

    Use custom element for product item

--- lib/ui/purchase_list_item.dart
+++ lib/ui/purchase_list_item.dart
@@ -12,22 +12,57 @@ class PurchaseListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final background =
+        purchase.purchased ? Colors.green.shade100 : Colors.white;
+
     return Container(
-        color: purchase.purchased ? Colors.green.shade100 : null,
-        child: ListTile(
-            contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-            title: Text(purchase.name),
-            leading: Padding(
-                padding: EdgeInsets.only(left: 8.0),
-                child: purchase.purchased
-                    ? const Icon(Icons.check, color: Colors.green)
-                    : const Icon(Icons.remove)),
-            trailing: onDelete != null
-                ? IconButton(
-                    icon: const Icon(Icons.delete),
-                    padding: const EdgeInsets.all(2.0),
-                    onPressed: onDelete)
-                : null,
-            onTap: onTap));
+        height: 60,
+        child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+              child: Material(
+                  color: background,
+                  child: InkWell(
+                      onTap: onTap,
+                      child: Row(children: [
+                        _PurchasedIcon(purchased: purchase.purchased),
+                        Expanded(
+                            child: Text(purchase.name,
+                                style: const TextStyle(fontSize: 16)))
+                      ])))),
+          Material(
+              color: background,
+              child: InkWell(
+                  onTap: onDelete,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Opacity(
+                          opacity: 0.5,
+                          child: const Icon(Icons.delete, size: 20)))))
+        ]));
+  }
+}
+
+class _PurchasedIcon extends StatelessWidget {
+  final bool purchased;
+
+  _PurchasedIcon({Key key, @required this.purchased}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Stack(children: [
+        AnimatedOpacity(
+            curve: Curves.ease,
+            duration: Duration(milliseconds: 200),
+            opacity: purchased ? 1 : 0,
+            child: Icon(Icons.check, color: Colors.green)),
+        AnimatedOpacity(
+            curve: Curves.ease,
+            duration: Duration(milliseconds: 200),
+            opacity: purchased ? 0 : 1,
+            child: Icon(Icons.remove))
+      ]),
+    );
   }
 }
