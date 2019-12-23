a5cfc9e394b6bcaec8d5162cd35678a25cec07d2
Albert221 <w.albert221@gmail.com>
2019-12-23 22:30:10 +0100

    Initial commit for 2.0

---
+++ android/app/src/main/kotlin/me/wolszon/fast_shopping/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.wolszon.fast_shopping
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
