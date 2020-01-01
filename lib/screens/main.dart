b35906262fe0a55fadb5002ce71874b9d2413b76
Albert221 <w.albert221@gmail.com>
2020-01-01 17:45:40 +0100

    Add basic state handling to main screen

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,7 +1,26 @@
 import 'package:fast_shopping/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _items = [
+    Item('Herbatniki duża paczka'),
+    Item('3x bita śmietana (proszek)'),
+    Item('0,5l śmietany 30% karton'),
+    Item('Krem karpatka proszek'),
+    Item(
+        'Masa kajmakowa/krówkowa (puszka) albo mleko skondensowane jak nie będzie'),
+    Item('Kapusta czerwona 2x średnie'),
+    Item('6 cebul czerwonych'),
+  ];
+
+  bool _shouldShowFab(BuildContext context) =>
+      MediaQuery.of(context).viewInsets.bottom == 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,22 +33,56 @@ class MainScreen extends StatelessWidget {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
+      floatingActionButton: _shouldShowFab(context)
+          ? FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {},
+            )
+          : null,
       body: ListView.builder(
         padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-        itemCount: 4,
-        itemBuilder: (context, i) => Padding(
-          padding: const EdgeInsets.only(bottom: 16),
-          child: ListItem(
-            done: true,
-            doneAt: DateTime.now().subtract(Duration(hours: 2)),
-            title: 'Herbatniki duża paczka',
-          ),
-        ),
+        itemCount: _items.length,
+        itemBuilder: (context, i) {
+          final item = _items[i];
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: ListItem(
+              key: item.widgetKey,
+              title: item.title,
+              done: item.done,
+              doneAt: item.doneAt,
+              onDoneTap: (value) {
+                setState(() {
+                  item.done = value;
+                  if (value) {
+                    item.doneAt = DateTime.now();
+                  } else {
+                    item.doneAt = null;
+                  }
+                });
+              },
+              onTitleEdited: (newTitle) {
+                setState(() => item.title = newTitle);
+              },
+              onExpand: () {
+                _items.where((a) => a != item).forEach((otherItem) {
+                  otherItem.widgetKey.currentState.collapse();
+                });
+              },
+            ),
+          );
+        },
       ),
     );
   }
+}
+
+class Item {
+  final GlobalKey<ListItemState> widgetKey;
+
+  String title;
+  bool done;
+  DateTime doneAt;
+
+  Item(this.title, [this.done = false, this.doneAt]) : widgetKey = GlobalKey();
 }
--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -7,8 +7,10 @@ class ListItem extends StatefulWidget {
   final DateTime doneAt;
 
   final bool actionsVisible;
+  final ValueChanged<bool> onDoneTap;
+  final ValueChanged<String> onTitleEdited;
   final VoidCallback onDeleteTap;
-  final ValueChanged<String> onEdited;
+  final VoidCallback onExpand;
 
   const ListItem({
     Key key,
@@ -16,216 +18,229 @@ class ListItem extends StatefulWidget {
     @required this.title,
     this.doneAt,
     this.actionsVisible = true,
+    this.onDoneTap,
+    this.onTitleEdited,
     this.onDeleteTap,
-    this.onEdited,
+    this.onExpand,
   }) : super(key: key);
 
   @override
-  _ListItemState createState() => _ListItemState();
+  ListItemState createState() => ListItemState();
 }
 
-class _ListItemState extends State<ListItem>
+class ListItemState extends State<ListItem>
     with SingleTickerProviderStateMixin {
   final _titleController = TextEditingController();
   final _titleFocusNode = FocusNode();
 
-  bool expanded = false;
-  bool editing = false;
+  bool _expanded = false;
+  bool _editing = false;
 
   @override
   void initState() {
     super.initState();
-    _setupTitle();
+    _resetTitle();
   }
 
-  void _setupTitle() {
+  void _resetTitle() {
     _titleController
       ..text = widget.title
       ..selection = TextSelection.collapsed(offset: widget.title.length);
   }
 
-  void _onEditingComplete() {
-    if (widget.onEdited != null) {
-      widget.onEdited(_titleController.text);
+  void _onEditingTitleComplete() {
+    setState(() => _editing = false);
+    if (widget.onTitleEdited != null) {
+      widget.onTitleEdited(_titleController.text);
     }
-    setState(() => editing = false);
+  }
+
+  void _cancelEditingTitle() {
+    _resetTitle();
+    setState(() => _editing = false);
+  }
+
+  void expand() {
+    setState(() => _expanded = true);
+    if (widget.onExpand != null) {
+      widget.onExpand();
+    }
+  }
+
+  void collapse() {
+    // FocusScope.of(context).unfocus();
+    _cancelEditingTitle();
+    setState(() => _expanded = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    const curve = Curves.ease;
-    const duration = const Duration(milliseconds: 300);
-
     return AnimatedCrossFade(
-      firstCurve: curve,
-      secondCurve: curve,
-      sizeCurve: curve,
-      duration: duration,
+      firstCurve: Curves.ease,
+      secondCurve: Curves.ease,
+      sizeCurve: Curves.ease,
+      duration: const Duration(milliseconds: 300),
       crossFadeState:
-          expanded ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-      firstChild: Container(
-        margin: EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black12),
-          borderRadius: BorderRadius.circular(4),
-        ),
-        child: Material(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(4),
-          child: InkWell(
-            onTap: () => setState(() => expanded = true),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Checkbox(
-                  visualDensity: VisualDensity.standard,
-                  value: widget.done,
-                  onChanged: (_) {},
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 16, 16, 16),
-                    child: Text(widget.title),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+          _expanded ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+      firstChild: _buildCollapsed(context),
+      secondChild: _buildExpanded(context),
+    );
+  }
+
+  Widget _buildCollapsed(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12),
+        borderRadius: BorderRadius.circular(4),
       ),
-      secondChild: Container(
-        decoration: BoxDecoration(
-          border: const Border(
-            top: BorderSide(color: Colors.black12),
-            bottom: BorderSide(color: Colors.black12),
-          ),
-        ),
-        child: Material(
-          color: Colors.white,
-          child: Column(
+      child: Material(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4),
+        child: InkWell(
+          onTap: expand,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  const SizedBox(width: 17),
-                  Checkbox(
-                    visualDensity: VisualDensity.standard,
-                    value: widget.done,
-                    onChanged: (_) {},
-                  ),
-                  if (widget.doneAt != null)
-                    Text(
-                      'oznaczone ' + timeago.format(widget.doneAt),
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      setState(() {
-                        expanded = false;
-                        editing = false;
-                        _setupTitle();
-                      });
-                    },
-                  ),
-                ],
+              Checkbox(
+                visualDensity: VisualDensity.standard,
+                value: widget.done,
+                onChanged: widget.onDoneTap,
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 32, vertical: 14),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.zero,
-                  ),
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.done,
-                  minLines: 1,
-                  maxLines: 1000,
-                  readOnly: !editing,
-                  controller: _titleController,
-                  focusNode: _titleFocusNode,
-                  onSubmitted: (_) => _onEditingComplete(),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 16, 16, 16),
+                  child: Text(widget.title),
                 ),
               ),
-              if (widget.actionsVisible)
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 16, right: 16, bottom: 12),
-                  child: editing
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            FlatButton(
-                              child: Text('ANULUJ'),
-                              onPressed: () {
-                                setState(() {
-                                  editing = false;
-                                  _setupTitle();
-                                });
-                              },
-                            ),
-                            const SizedBox(width: 16),
-                            FlatButton(
-                              child: Text(
-                                'ZAPISZ',
-                                style: TextStyle(
-                                  color: Color(0xFF4D3900),
-                                ),
-                              ),
-                              color: Color(0xFFFFF0C3),
-                              onPressed: _onEditingComplete,
-                            ),
-                          ],
-                        )
-                      : Row(
-                          children: [
-                            GestureDetector(
-                              // TODO: Dragging
-                              child: Icon(
-                                Icons.drag_handle,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            Spacer(),
-                            FlatButton(
-                              child: Text(
-                                'USUŃ',
-                                style: TextStyle(
-                                  color: Color(0xFFD32F2F),
-                                ),
-                              ),
-                              onPressed: () {
-                                if (widget.onDeleteTap != null) {
-                                  widget.onDeleteTap();
-                                }
-                              },
-                            ),
-                            const SizedBox(width: 16),
-                            FlatButton(
-                                child: Text(
-                                  'EDYTUJ',
-                                  style: TextStyle(
-                                    color: Color(0xFF4D3900),
-                                  ),
-                                ),
-                                color: Color(0xFFFFF0C3),
-                                onPressed: () {
-                                  FocusScope.of(context)
-                                      .requestFocus(_titleFocusNode);
-                                  setState(() => editing = true);
-                                }),
-                          ],
-                        ),
-                )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildExpanded(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Colors.black12),
+          bottom: BorderSide(color: Colors.black12),
+        ),
+      ),
+      child: Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const SizedBox(width: 17),
+                Checkbox(
+                  visualDensity: VisualDensity.standard,
+                  value: widget.done,
+                  onChanged: widget.onDoneTap,
+                ),
+                if (widget.doneAt != null)
+                  Text(
+                    'oznaczone ' + timeago.format(widget.doneAt),
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black87,
+                    ),
+                  ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  onPressed: collapse,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 14),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.zero,
+                ),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                minLines: 1,
+                maxLines: 1000,
+                readOnly: !_editing,
+                controller: _titleController,
+                focusNode: _titleFocusNode,
+                onSubmitted: (_) => _onEditingTitleComplete(),
+              ),
+            ),
+            if (widget.actionsVisible)
+              Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16, bottom: 12),
+                child: _editing
+                    ? _buildEditingActions(context)
+                    : _buildItemActions(context),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEditingActions(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FlatButton(
+          child: Text('ANULUJ'),
+          onPressed: _cancelEditingTitle,
+        ),
+        const SizedBox(width: 16),
+        FlatButton(
+          child: Text(
+            'ZAPISZ',
+            style: const TextStyle(color: Color(0xFF4D3900)),
+          ),
+          color: const Color(0xFFFFF0C3),
+          onPressed: _onEditingTitleComplete,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildItemActions(BuildContext context) {
+    return Row(
+      children: [
+        GestureDetector(
+          // TODO: Dragging
+          child: const Icon(
+            Icons.drag_handle,
+            color: Colors.black87,
+          ),
+        ),
+        Spacer(),
+        FlatButton(
+          child: Text(
+            'USUŃ',
+            style: const TextStyle(color: Color(0xFFD32F2F)),
+          ),
+          onPressed: () {
+            if (widget.onDeleteTap != null) {
+              widget.onDeleteTap();
+            }
+          },
+        ),
+        const SizedBox(width: 16),
+        FlatButton(
+          child: Text(
+            'EDYTUJ',
+            style: const TextStyle(color: Color(0xFF4D3900)),
+          ),
+          color: const Color(0xFFFFF0C3),
+          onPressed: () {
+            FocusScope.of(context).requestFocus(_titleFocusNode);
+            setState(() => _editing = true);
+          },
+        ),
+      ],
     );
   }
 }
