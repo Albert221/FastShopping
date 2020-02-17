ccda48e38670b3105b88b25d3de97b6c1ea00727
Albert221 <w.albert221@gmail.com>
2020-02-17 19:19:09 +0100

    Move i18n messages to lib/i18n/messages

--- lib/i18n/internationalization.dart
+++ lib/i18n/internationalization.dart
@@ -16,21 +16,22 @@ class FastShoppingI18n {
   Future<void> init([String preferredLocale = 'en_US']) async {
     if (preferredLocale != 'en_US') {
       try {
-        final json =
-            await rootBundle.loadString('lib/i18n/$preferredLocale.json');
+        final json = await rootBundle
+            .loadString('lib/i18n/messages/$preferredLocale.json');
         _preferredMessages = Map<String, String>.from(jsonDecode(json));
       } catch (_) {
         try {
           final language = preferredLocale.split('_').first;
 
-          final json = await rootBundle.loadString('lib/i18n/$language.json');
+          final json =
+              await rootBundle.loadString('lib/i18n/messages/$language.json');
           _preferredMessages = Map<String, String>.from(jsonDecode(json));
-        } catch (__) {}
+        } catch (_) {}
       }
     }
 
     final fallbackJson =
-        await rootBundle.loadString('lib/i18n/$_fallbackLocale.json');
+        await rootBundle.loadString('lib/i18n/messages/$_fallbackLocale.json');
     _fallbackMessages = Map<String, String>.from(jsonDecode(fallbackJson));
   }
 
