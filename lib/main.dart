c18b98f445c242f36c902470bc3b90abe43442a9
Albert221 <w.albert221@gmail.com>
2020-02-18 20:01:20 +0100

    Add data migration from v1 feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:built_collection/built_collection.dart';
 import 'package:devicelocale/devicelocale.dart';
 import 'package:fast_shopping/app.dart';
 import 'package:fast_shopping/i18n/i18n.dart';
@@ -7,18 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
-import 'package:uuid/uuid.dart';
-import 'models/models.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await FastShoppingI18n().init(await Devicelocale.currentLocale);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   final store = _setupStore();
-
-  final lang = await Devicelocale.currentLocale;
-  await FastShoppingI18n().init(lang);
-
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(FastShoppingApp(store: store));
 }
@@ -34,42 +31,3 @@ Store<FastShoppingState> _setupStore() {
     // initialState: _initialState,
   )..dispatch(LoadData());
 }
-
-final _initialListId = Uuid().v4();
-final _initialState = FastShoppingState(
-  (b) => b
-    ..currentListId = _initialListId
-    ..lists = BuiltList<ShoppingList>([
-      ShoppingList(
-        (b) => b
-          ..id = _initialListId
-          ..name = 'My shopping list',
-      )
-    ]).toBuilder()
-    ..items = BuiltList<Item>([
-      Item((b) => b
-        ..title = 'Herbatniki duża paczka'
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = '3x bita śmietana (proszek)'
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = '0,5l śmietany 30% karton'
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = 'Krem karpatka proszek'
-        ..done = true
-        ..doneAt = DateTime.now()
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = 'Masa kajmakowa/krówkowa (puszka) '
-            'albo mleko skondensowane jak nie będzie'
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = 'Kapusta czerwona 2x średnie'
-        ..shoppingListId = _initialListId),
-      Item((b) => b
-        ..title = '6 cebul czerwonych'
-        ..shoppingListId = _initialListId),
-    ]).toBuilder(),
-);
--- lib/store/persister.dart
+++ lib/store/persister.dart
@@ -3,10 +3,12 @@ import 'dart:convert';
 import 'package:built_value/serializer.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/standard_json_plugin.dart';
+import 'package:fast_shopping/i18n/i18n.dart';
 import 'package:fast_shopping/models/models.dart';
 import 'package:fast_shopping/store/store.dart';
 import 'package:redux/redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 part 'persister.g.dart';
 
@@ -19,6 +21,7 @@ class LoadedData {
 }
 
 Middleware<FastShoppingState> persisterMiddleware() {
+  const v1DataKey = 'DATA';
   const dataKey = 'V2-DATA';
   final sharedPrefs = SharedPreferences.getInstance();
 
@@ -26,20 +29,33 @@ Middleware<FastShoppingState> persisterMiddleware() {
     next(action);
 
     if (action is LoadData) {
-      // Load data
       sharedPrefs.then((prefs) {
-        if (prefs.containsKey(dataKey)) {
+        // Check for data from v1.x version of the app
+        if (prefs.containsKey(v1DataKey)) {
+          final json = prefs.getString(v1DataKey);
+          final migratedState = _migrateV1ToV2(json);
+
+          store.dispatch(LoadedData(migratedState));
+        } else if (prefs.containsKey(dataKey)) {
+          // Load data
           final json = prefs.getString(dataKey);
           final loadedState = _serializers.deserializeWith(
-              FastShoppingState.serializer, jsonDecode(json));
+            FastShoppingState.serializer,
+            jsonDecode(json),
+          );
 
           store.dispatch(LoadedData(loadedState));
         }
       });
     } else {
       // Save data
-      final json = jsonEncode(_serializers.serializeWith(
-          FastShoppingState.serializer, store.state));
+      final json = jsonEncode(
+        _serializers.serializeWith(
+          FastShoppingState.serializer,
+          store.state,
+        ),
+      );
+
       sharedPrefs.then((prefs) => prefs.setString(dataKey, json));
     }
   };
@@ -48,3 +64,31 @@ Middleware<FastShoppingState> persisterMiddleware() {
 @SerializersFor(const [FastShoppingState])
 final Serializers _serializers =
     (_$_serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
+
+FastShoppingState _migrateV1ToV2(String json) {
+  final purchases = jsonDecode(json) as List<Map<String, dynamic>>;
+
+  final list = ShoppingList(
+    (b) => b
+      ..id = Uuid().v4()
+      ..name = 'list_from_old_version'.i18n,
+  );
+
+  final items = purchases.map((purchase) {
+    final done = purchase['purchased'] as bool;
+
+    return Item(
+      (b) => b
+        ..title = purchase['name'] as String
+        ..done = done
+        ..doneAt = done ? DateTime.now().toUtc() : null,
+    );
+  });
+
+  return FastShoppingState(
+    (b) => b
+      ..currentListId = list.id
+      ..lists = BuiltList<ShoppingList>([list]).toBuilder()
+      ..items = BuiltList<Item>(items).toBuilder(),
+  );
+}
