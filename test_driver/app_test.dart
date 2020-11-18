d02ff167abafa875205f17db2fee12243bca63ce
Albert221 <w.albert221@gmail.com>
2020-11-18 23:28:01 +0100

    Add feature graphic screenshot automation

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -19,14 +19,30 @@ void main() {
       test(locale, () async {
         await driver.requestData('locale-$locale');
         await Future.delayed(const Duration(seconds: 2));
+
         await driver
             .tap(find.byValueKey('21827905-af88-4ff2-88a1-490e40d835d8'));
-        await _saveScreenshot(locale, '1', driver.screenshot());
+
+        final screenshot1 = await driver.screenshot();
+        await _saveScreenshot(locale, '1', screenshot1);
+
         await driver.tap(find.byType('FloatingActionButton'));
-        await _saveScreenshot(locale, '2', driver.screenshot());
+
+        final screenshot2 = await driver.screenshot();
+        await _saveScreenshot(locale, '2', screenshot2);
+
         await driver.tap(find.byValueKey('add_item_back'));
         await driver.tap(find.byType('_BottomAppBar'));
-        await _saveScreenshot(locale, '3', driver.screenshot());
+
+        final screenshot3 = await driver.screenshot();
+        await _saveScreenshot(locale, '3', screenshot3);
+
+        await _saveFeatureGraphic(
+          locale,
+          screenshot1: screenshot1,
+          screenshot2: screenshot2,
+          screenshot3: screenshot3,
+        );
       });
     }
   });
@@ -35,9 +51,9 @@ void main() {
 Future<void> _saveScreenshot(
   String locale,
   String name,
-  Future<List<int>> bytes,
+  List<int> bytes,
 ) async {
-  final screenshot = decodePng(await bytes);
+  final screenshot = decodePng(bytes);
   final screenshotFrame = decodePng(
     await File('assets/screenshot-frame.png').readAsBytes(),
   );
@@ -49,13 +65,82 @@ Future<void> _saveScreenshot(
     image,
     width: 1080,
     height: 1920,
-    interpolation: Interpolation.cubic,
+    interpolation: Interpolation.average,
   );
 
   final resultBytes = encodePng(image);
 
+  final file = File(
+      'fastlane/metadata/android/$locale/images/phoneScreenshots/$name.png');
+  await file.create(recursive: true);
+  await file.writeAsBytes(resultBytes);
+}
+
+Future<void> _saveFeatureGraphic(
+  String locale, {
+  List<int> screenshot1,
+  List<int> screenshot2,
+  List<int> screenshot3,
+}) async {
+  var image = Image(1024, 500);
+  image = fill(image, getColor(0xFF, 0xC1, 0x07));
+
+  final phoneFrame =
+      decodePng(await File('assets/phone-frame.png').readAsBytes());
+  final phoneFrameBlank = Image(1623, 3322);
+
+  var first = drawImage(
+    phoneFrameBlank,
+    decodePng(screenshot1),
+    dstX: 95,
+    dstY: 223,
+  );
+  first = drawImage(first, phoneFrame);
+  first = copyCrop(first, 0, 0, 1623, 2932);
+  first = copyResize(
+    first,
+    width: 235,
+    height: 424,
+    interpolation: Interpolation.average,
+  );
+  image = drawImage(image, first, dstX: 70, dstY: 76);
+
+  var second = drawImage(
+    phoneFrameBlank,
+    decodePng(screenshot2),
+    dstX: 95,
+    dstY: 223,
+  );
+  second = drawImage(second, phoneFrame);
+  second = copyCrop(second, 0, 0, 1623, 2932);
+  second = copyResize(
+    second,
+    width: 235,
+    height: 424,
+    interpolation: Interpolation.average,
+  );
+  image = drawImage(image, second, dstX: 396, dstY: 76);
+
+  var third = drawImage(
+    phoneFrameBlank,
+    decodePng(screenshot3),
+    dstX: 95,
+    dstY: 223,
+  );
+  third = drawImage(third, phoneFrame);
+  third = copyCrop(third, 0, 0, 1623, 2932);
+  third = copyResize(
+    third,
+    width: 235,
+    height: 424,
+    interpolation: Interpolation.average,
+  );
+  image = drawImage(image, third, dstX: 718, dstY: 76);
+
+  final resultBytes = encodePng(image);
+
   final file =
-      File('metadata/android/$locale/images/phoneScreenshots/$name.png');
+      File('fastlane/metadata/android/$locale/images/featureGraphic.png');
   await file.create(recursive: true);
   await file.writeAsBytes(resultBytes);
 }
