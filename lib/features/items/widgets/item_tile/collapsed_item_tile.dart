5be881ccb9a8fd5b8884b5cfdde1519b3c287bb3
Albert221 <w.albert221@gmail.com>
2020-12-12 21:28:15 +0100

    Fix bleeding material from collapsed item tile

--- lib/features/items/widgets/item_tile/collapsed_item_tile.dart
+++ lib/features/items/widgets/item_tile/collapsed_item_tile.dart
@@ -24,40 +24,43 @@ class CollapsedItemTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Material(
-        color: Theme.of(context).colorScheme.surface,
-        child: InkWell(
-          onTap: () => onExpandedChanged?.call(true),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black12),
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 1),
-                  child: Checkbox(
-                    value: item.doneAt != null,
-                    onChanged: onDoneChanged,
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 16, 16, 16),
-                    child: AnimatedLineThroughText(
-                      item.title.isNotEmpty
-                          ? item.title
-                          : S.of(context).list_item_no_name,
-                      stroked: item.doneAt != null,
-                      duration: const Duration(milliseconds: 300),
-                      curve: standardEasing,
-                      style: item.title.isEmpty ? _emptyTitleStyle : null,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(4),
+        child: Material(
+          color: Theme.of(context).colorScheme.surface,
+          child: InkWell(
+            onTap: () => onExpandedChanged?.call(true),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 1),
+                    child: Checkbox(
+                      value: item.doneAt != null,
+                      onChanged: onDoneChanged,
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 16, 16, 16),
+                      child: AnimatedLineThroughText(
+                        item.title.isNotEmpty
+                            ? item.title
+                            : S.of(context).list_item_no_name,
+                        stroked: item.doneAt != null,
+                        duration: const Duration(milliseconds: 300),
+                        curve: standardEasing,
+                        style: item.title.isEmpty ? _emptyTitleStyle : null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
