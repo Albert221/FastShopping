12f4056d8b9dc33b86c848d441488814c4e45ff1
Albert221 <w.albert221@gmail.com>
2021-01-14 18:58:18 +0100

    Add animating removals of shopping lists

--- lib/features/shopping_lists/widgets/tabs/shopping_lists_tab.dart
+++ lib/features/shopping_lists/widgets/tabs/shopping_lists_tab.dart
@@ -1,5 +1,7 @@
 import 'package:fast_shopping_bloc/models.dart';
 import 'package:flutter/material.dart';
+import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
+import 'package:implicitly_animated_reorderable_list/transitions.dart';
 
 class ShoppingListsTab extends StatelessWidget {
   const ShoppingListsTab({
@@ -13,17 +15,18 @@ class ShoppingListsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ImplicitlyAnimatedList<ShoppingList>(
+      items: shoppingLists,
+      areItemsTheSame: (oldItem, newItem) => oldItem.id == newItem.id,
       padding: const EdgeInsets.symmetric(vertical: 8),
-      itemCount: shoppingLists.length,
-      itemBuilder: (context, i) {
-        final shoppingList = shoppingLists[i];
-
-        return Padding(
+      itemBuilder: (context, itemAnimation, item, index) => SizeFadeTransition(
+        curve: standardEasing,
+        animation: itemAnimation,
+        child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 4),
-          child: itemBuilder(shoppingList),
-        );
-      },
+          child: itemBuilder(item),
+        ),
+      ),
     );
   }
 }
